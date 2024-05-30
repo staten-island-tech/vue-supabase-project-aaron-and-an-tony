844df70b68975827756e02383680304bba459809
AnthonyPvZ <anthonypvz123@gmail.com>
2024-05-30 09:21:31 -0400

    m

---
+++ vite-project/src/components/gallery.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="card">
+      <img :src="pics.imageurls" alt=""/>
+    </div>
+    </template>
+      
+<script setup>
+import { onMounted, ref } from 'vue';
+import { supabase } from '../supabase';
+const pics = ref(getimages);
+async function getimages(){
+  const { data, error } = await supabase.from('pictures').select('*')
+  if (error) {
+    console.error('Error fetching data:', error);
+    return;
+  }
+  console.log(data)
+  return data
+}
+
+onMounted(()=>(getimages()))
+
+</script>
+    
+<style scoped>
+    
+</style>
--- vite-project/src/views/stereograms.vue
+++ vite-project/src/views/stereograms.vue
@@ -1,50 +1,10 @@
 <template>
 <div class="images">
-  <div id="container"></div>
+  <gallery
+  />
 </div> 
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import { supabase } from '../supabase';
-let pics = ref('getimages');
-async function getimages(){
-  const { data, error } = await supabase.from('pictures').select('*')
-  if (error) {
-    console.error('Error fetching data:', error);
-    return;
-  }
-  pics = data
-  console.log(data)
-
-  let x = Array.from(pics);
-        function create(blah) {
-            blah.map((result) => {
-                const correct = result.answer
-                function check(){
-                  if (input = correct) {
-                    then //add picture to profile
-                  } else {
-                    return //WRONG
-                  }
-                }
-                const card = document.createElement('div');
-                card.classlist = 'card'
-                const desc = `<div class=card>
-                <img class="card" alt ="Stereogram" src="${result.imageurls}"></div>`
-                container.innerHTML += desc;
-            })
-        }
-        create(x)
-        console.log(x)
-}
-onMounted(()=>(getimages()))
-
+import gallery from '../components/gallery.vue'
 </script>
-
-<style scoped>
-.card {
-  max-width: 100px;
-  height: 60px;
-}
-</style>  

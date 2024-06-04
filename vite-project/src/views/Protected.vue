f65817dfd4a234951030dfe4c4f4889763e43ff7
AnthonyPvZ <anthonypvz123@gmail.com>
2024-06-04 18:03:10 -0400

    WOWWWWWWWWWWWWWWW

--- vite-project/src/views/Protected.vue
+++ vite-project/src/views/Protected.vue
@@ -1,34 +1,28 @@
 <template>
     <div>
-    {{ userSession.session }}
-    </div>
+      {{ userSession.session }}
+    </div>    
     <button className="p-4 bg-blue-400 text-white rounded-xl hover:bg-blue-500" @click="logOut">
-        Sign Out
+      Sign Out
     </button>
+
 </template>
-
-<script>
-import { supabase } from '../supabase.js';
-import router from '../router'
-import {ref} from 'vue'
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
-import App from '../App.vue'
-import { userSessionStore } from '../stores/userSession.js';
-// initialize userSession store
-const pinia = createPinia()
-const app = createApp(App)
-app.use(pinia)
-
-const userSession = ref((userSessionStore()))
-
-// logout function
-const logOut = async () => {
+  
+  <script setup>
+  import { supabase } from '../supabase.js';
+  import { userSessionStore } from '../stores/userSession.js';
+  import {ref} from 'vue'
+  import router from '../router';
+  // initialize userSession store
+  const userSession = userSessionStore()
+  // logout function
+  const logOut = async () => {
     try {
-        const { error } = await supabase.auth.signOut().then(router.push('/'))
-        if (error) throw error
-    } catch (error) {
-        alert(error.message)
+      const { error } = await supabase.auth.signOut().then(router.push('/'))
+      if(error) throw error
+    } catch(error){
+      alert(error.message)
     }
-}
-</script>
+  }
+
+  </script>
